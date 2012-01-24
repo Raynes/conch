0d6a563eacfaae88e53403d2dd76026f2764a3ed
Anthony Grimes <i@raynes.me>
2012-01-24 17:25:36 -0600

    Documented things better, added more utils, and got feeding working.

--- src/conch/core.clj
+++ src/conch/core.clj
@@ -1,4 +1,5 @@
 (ns conch.core
+  "A simple but flexible library for shelling out from Clojure."
   (:refer-clojure :exclude [flush])
   (:require [clojure.java.io :as io]))
 
@@ -13,8 +14,8 @@
 
 (defn proc
   "Spin off another process. Returns the process's input stream,
-  output stream, and err stream as a map of :in, :out, and :err keys. 
-  If passed the optional :dir and/or :env keyword options, the dir 
+  output stream, and err stream as a map of :in, :out, and :err keys
+  If passed the optional :dir and/or :env keyword options, the dir
   and enviroment will be set to what you specify."
   [& args]
   (let [[cmd args] (split-with (complement keyword?) args)
@@ -29,6 +30,24 @@
      :err (.getErrorStream process)
      :process process}))
 
+(defn destroy
+  "Destroy a process. Kills the process and closes its streams."
+  [process]
+  (doseq [[_ std-stream] (dissoc process :process)]
+    (.close std-stream))
+  (.destroy (:process process)))
+
+(defn exit-code
+  "Waits for the process to terminate (blocking the thread) and returns
+  the exit code."
+  [process]
+  (.waitFor (:process process)))
+
+(defn flush
+  "Flush the output stream of a process."
+  [process]
+  (.flush (:in process)))
+
 (defn stream-to
   "Stream :out or :err from a process to an input stream.
   If :enc option is specified, stream with that encoding. The
@@ -37,19 +56,27 @@
   (io/copy (io/reader (process from)) to :encoding enc))
 
 (defn feed-from
-  "Feed to a process's input stream with optional :enc encoding."
-  [process from & {:keys [enc] :or {enc "UTF-8"}}]
-  (io/copy from (io/writer (:in process)) :encoding enc))
+  "Feed to a process's input stream with optional :enc encoding. if
+  :flush isn't specified as false, flushes the output stream after
+  writing."
+  [process from & {:keys [enc flush] :or {enc "UTF-8", flush true}}]
+  (io/copy from (:in process) :encoding enc)
+  (when flush (conch.core/flush process)))
 
-(defn stream-to-string [process from & args]
+(defn stream-to-string
+  "Streams the output of the process to a string and returns it."
+  [process from & args]
   (with-open [writer (java.io.StringWriter.)]
     (apply stream-to process from writer args)
     (str writer)))
 
-(defn stream-to-out [process from & args]
+(defn stream-to-out
+  "Streams the output of the process to *out*."
+  [process from & args]
   (apply stream-to process from *out* args))
 
-(defn feed-from-string [process s & args]
-  (binding [*out* (io/writer (:in process))]
-    (println s)))
+(defn feed-from-string
+  "Feed the process some data from a string."
+  [process s & args]
+  (apply feed-from process (java.io.StringReader. s) args))
 
