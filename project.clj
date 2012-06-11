edce7e3b13649c85412c6d4d3c4d5a8ecacadc73
Anthony Grimes <i@raynes.me>
2012-06-10 19:13:34 -0500

    Inc to 0.3.0 and test against multiple Clojure versions.

--- project.clj
+++ project.clj
@@ -1,3 +1,6 @@
-(defproject conch "0.2.4"
+(defproject conch "0.3.0"
   :description "A better shell-out library for Clojure."
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :aliases {"test-all" ["with-profile" "dev,default:dev,1.2,default:dev,1.3,default" "test"]}
+  :profiles {:1.2 {:dependencies [[org.clojure/clojure "1.2.1"]]}
+             :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}})
