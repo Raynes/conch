1f467943b4572e602aa1cdbf5f7913d71d519a86
Anthony Grimes <i@raynes.me>
2012-07-30 17:31:15 -0500

    Test against Clojure 1.5.0 snapshots and add :url and :license keys.

--- project.clj
+++ project.clj
@@ -1,6 +1,13 @@
 (defproject conch "0.3.0"
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :url "https://github.com/Raynes/conch"
   :description "A better shell-out library for Clojure."
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :aliases {"test-all" ["with-profile" "dev,default:dev,1.2,default:dev,1.3,default" "test"]}
   :profiles {:1.2 {:dependencies [[org.clojure/clojure "1.2.1"]]}
-             :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}})
+             :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]}}
+  :repositories {"sonatype-snapshots" {:url "http://oss.sonatype.org/content/repositories/snapshots"
+                                       :snapshots true
+                                       :releases {:checksum :fail :update :always}}})
