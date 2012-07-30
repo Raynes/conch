d0e595f129327d76195da9330cb7ec28ede894ca
Anthony Grimes <i@raynes.me>
2012-07-30 17:33:43 -0500

    Update alias for 1.5 test.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :url "https://github.com/Raynes/conch"
   :description "A better shell-out library for Clojure."
   :dependencies [[org.clojure/clojure "1.4.0"]]
-  :aliases {"test-all" ["with-profile" "dev,default:dev,1.2,default:dev,1.3,default" "test"]}
+  :aliases {"test-all" ["with-profile" "dev,default:dev,1.2,default:dev,1.3,default:dev,1.5,default" "test"]}
   :profiles {:1.2 {:dependencies [[org.clojure/clojure "1.2.1"]]}
              :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]}}
