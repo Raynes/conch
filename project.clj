c333ab50978ef415ca79a5c582b8e49effa94794
Anthony Grimes <i@raynes.me>
2012-09-23 21:52:34 -0500

    Add let-programs and with-programs.

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :url "https://github.com/Raynes/conch"
   :description "A better shell-out library for Clojure."
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [useful "0.8.5-alpha2"]]
   :aliases {"testall" ["with-profile" "dev,default:dev,1.2,default:dev,1.3,default:dev,1.5,default" "test"]}
   :profiles {:1.2 {:dependencies [[org.clojure/clojure "1.2.1"]]}
              :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
