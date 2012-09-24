aaf607f668c4df76eb5d550f1397490f681765a1
Anthony Grimes <i@raynes.me>
2012-09-23 21:52:34 -0500

    Add let-programs and with-programs.

--- src/conch/sh.clj
+++ src/conch/sh.clj
@@ -1,7 +1,8 @@
 (ns conch.sh
   (require [conch.core :as conch]
            [clojure.java.io :as io]
-           [clojure.string :as string]))
+           [clojure.string :as string]
+           [useful.seq :as seq]))
 
 (defn char-seq [reader]
   (map char (take-while #(not= % -1) (repeatedly #(.read reader)))))
@@ -33,4 +34,21 @@
   "Creates functions corresponding to progams on the PATH, named by names."
   [& names]
   `(do ~@(for [name names]
-           `(defn ~name [& ~'args] (apply execute ~(str name) ~'args)))))
+           `(defn ~name [& ~'args]
+              (apply execute ~(str name) ~'args)))))
+
+(defn- program-form [prog]
+  `(fn [& args#] (apply execute ~prog args#)))
+
+(defmacro let-programs
+  "Like let, but expects bindings to be symbols to strings of paths to
+   programs."
+  [bindings & body]
+  `(let [~@(seq/map-nth (fn [v] (program-form v)) 1 2 bindings)]
+     ~@body))
+
+(defmacro with-programs
+  "Like programs, but only binds names in the scope of the with-programs call."
+  [programs & body]
+  `(let [~@(interleave programs (map (comp program-form str) programs))]
+     ~@body))
