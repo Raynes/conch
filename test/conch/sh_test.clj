5bea3bc697748a33e38acd0c5fea0c291e8c319f
Anthony Grimes <i@raynes.me>
2012-09-30 04:32:52 -0500

    Test :err and clean up tests a bit.

--- test/conch/sh_test.clj
+++ test/conch/sh_test.clj
@@ -3,16 +3,23 @@
   (:require [conch.sh :as sh]))
 
 (deftest output-test
-  (testing "By default, output is accumulated into a monolitic string"
-    (is (= "hi\n" (sh/with-programs [echo] (echo "hi")))))
-  (testing "Output can be a lazy sequence"
-    (is (= ["hi" "there"] (sh/with-programs [echo] (echo "hi\nthere" {:seq true})))))
-  (testing "Can redirect output to a file"
-    (let [output "hi\nthere\n"]
-      (sh/with-programs [echo] (echo "hi\nthere" {:out (java.io.File. "testfile")}))
-      (is (= output (slurp "testfile")))))
-  (testing "Can redirect output to a callback function"
-    (let [x (atom [])]
-      (sh/with-programs [echo]
-        (echo "hi\nthere" {:out (fn [line _] (swap! x conj line))}))
-      (is (= ["hi" "there"] @x)))))
+  (sh/let-programs [errecho "test/testfiles/errecho"]
+    (sh/with-programs [echo]
+      (testing "By default, output is accumulated into a monolitic string"
+        (is (= "hi\n" (echo "hi"))))
+      (testing "Output can be a lazy sequence"
+        (is (= ["hi" "there"] (echo "hi\nthere" {:seq true}))))
+      (testing "Can redirect output to a file"
+        (let [output "hi\nthere\n"
+              testfile "test/testfiles/foo"]
+          (echo "hi\nthere" {:out (java.io.File. testfile)})
+          (is (= output (slurp testfile)))
+          (errecho "hi\nthere" {:err (java.io.File. testfile)})
+          (is (= output (slurp testfile)))))
+      (testing "Can redirect output to a callback function"
+        (let [x (atom [])
+              ex (atom [])]
+          (echo "hi\nthere" {:out (fn [line _] (swap! x conj line))})
+          (is (= ["hi" "there"] @x))
+          (errecho "hi\nthere" {:err (fn [line _] (swap! ex conj line))})
+          (is (= ["hi" "there"] @ex)))))))
