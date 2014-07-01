38c0857e46c83b8da301a7b9d60d1339ffe97d40
Anthony Grimes <i@raynes.me>
2014-06-30 19:36:55 -0700

    Inc minor version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject me.raynes/conch "0.7.0"
+(defproject me.raynes/conch "0.8.0"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :url "https://github.com/Raynes/conch"
